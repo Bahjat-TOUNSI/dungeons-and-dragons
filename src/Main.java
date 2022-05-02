8ab0169b44e5bd9d3dbbf33a79809713dd663bc4
bahjat.tounsi <bahjattounsi1@hotmail.com>
2022-05-02 14:54:49 +0200

    Changed warrior name

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Mage mage = new Mage();
         System.out.println(mage.name);
+        Warrior warrior = new Warrior();
+        System.out.println(warrior.name);
     }
 }
--- src/Warrior.java
+++ src/Warrior.java
@@ -6,7 +6,7 @@ public class Warrior {
         String shield;
 
         public Warrior() {
-                this.name = "Atilla the hun";
+                this.name = "Atilla The Hun";
                 this.lifePoints = 10;
                 this.attack = 10;
                 this.weaponType = "Axe";
