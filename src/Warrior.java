ffa4250581da2243920c7d0f37524cf2f18b67df
bahjat.tounsi <bahjattounsi1@hotmail.com>
2022-05-02 14:24:35 +0200

    first commit

---
+++ src/Warrior.java
@@ -0,0 +1,9 @@
+public class Warrior {
+    public static void main(String[] args) {
+        String name;
+        Integer lifePoints;
+        Integer attack;
+        String weaponType;
+        Boolean shield;
+    }
+}
