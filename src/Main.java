c2b8244af284d787de9b985c15d127594e13df8c
bahjat.tounsi <bahjattounsi1@hotmail.com>
2022-05-03 16:41:57 +0200

    end of day

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner replay = new Scanner(System.in);
         StartGame start = new StartGame();
         start.launch();
+        System.out.println("Again? y or n");
+        String result = replay.nextLine();
+        if(result.equalsIgnoreCase("y")){
+            start.launch();
+        }
+        System.out.println("Sorry to see you go");
     }
 }
 
