87931b79c036a818bcbca2386680649847d411b0
bahjat.tounsi <bahjattounsi1@hotmail.com>
2022-05-03 10:27:29 +0200

    Start game working

---
+++ src/StartGame.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class StartGame {
+
+    public void launch() {
+        Scanner playOrQuit = new Scanner(System.in);
+        System.out.println("Press 1 to Play or 0 to Quit");
+        Integer userChoice = playOrQuit.nextInt();
+        if (userChoice == 1) {
+            System.out.println("let's play!!!");
+            Scanner playerChoice = new Scanner(System.in);
+            System.out.println("Choose your class, 1 for Warrior or 2 for Mage");
+            Integer chosenCharacter = playerChoice.nextInt();
+            if (chosenCharacter == 1) {
+                System.out.println("You chose the Warrior class!");
+            }if (chosenCharacter == 2) {
+                System.out.println("You chose the Mage class!");
+            }
+        }if (userChoice == 0){
+            System.out.println("Sorry to see you go");
+        } else if (userChoice != 1) {
+            System.out.println("Error Please choose 1 or 0");
+        }
+    }
+}
